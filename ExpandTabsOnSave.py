7bbf79ea37a936e6a08fa2ed47bb731ed6d7d426
Anatoly Bubenkov <bubenkoff@gmail.com>
2013-12-20 00:22:07 +0100

    docs updated

--- ExpandTabsOnSave.py
+++ ExpandTabsOnSave.py
@@ -1,8 +1,14 @@
-import sublime, sublime_plugin, os
+"""ExpandTabsOnSave SublimeText plugin."""
+import os
+
+import sublime
+import sublime_plugin
+
 
 class ExpandTabsOnSave(sublime_plugin.EventListener):
-    """Run ST's 'expand_tabs' command when saving a file."""
+    """Expant tabs on file save."""
 
     def on_pre_save(self, view):
+        """Run ST's 'expand_tabs' command when saving a file."""
         if view.settings().get('expand_tabs_on_save') == 1:
             view.window().run_command('expand_tabs')
