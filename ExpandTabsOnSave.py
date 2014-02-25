f452f1a457f10a2b3f31e099240f0169c4693ed0
Sander van Dragt <sander@vandragt.com>
2014-02-25 15:19:54 +0000

    Update ExpandTabsOnSave.py

--- ExpandTabsOnSave.py
+++ ExpandTabsOnSave.py
@@ -10,7 +10,7 @@ class ExpandTabsOnSave(sublime_plugin.EventListener):
 
     def on_pre_save(self, view):
         """Run ST's 'expand_tabs' command when saving a file."""
-        if view.settings().get('expand_tabs_on_save') == 1:
+        if view.settings().get('convert_tabspaces_on_save') == 1:
             if view.settings().get('translate_tabs_to_spaces') == 1:
                 view.window().run_command('expand_tabs')
             else:
